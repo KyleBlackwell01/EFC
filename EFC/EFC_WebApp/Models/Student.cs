20ebceaf36400740f268cfff11619dc8b6a2558d
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-11-20 09:39:39 +1100

    Added students page

---
+++ EFC/EFC_WebApp/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace EFC_WebApp.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
